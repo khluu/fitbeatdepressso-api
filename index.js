cbbbd410e978265ee0b25a7ed7369650be2937a3
Ian Perez <iperez@mit.edu>
2020-09-19 15:02:44 -0400

    Fixed minor bug

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ index.use(session({
 }))
 
 index.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Origin", 'https://fit-beat-depresso.herokuapp.com');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
