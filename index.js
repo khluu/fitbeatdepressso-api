cd876e2e1facb98be22f7d6d95f8c9c1ac996e75
Ian Perez <iperez@mit.edu>
2020-09-19 14:57:54 -0400

    Fixed CORS issue

--- index.js
+++ index.js
@@ -108,7 +108,15 @@ index.use(session({
   saveUninitialized: true
 }))
 
-index.use(cors);
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
+// index.use(cors);
 index.use(bodyParser.json());
 index.use(morgan('combined'))
 index.use(passport.initialize());
